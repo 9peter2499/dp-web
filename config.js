99a935f555bd808f73e7d309e6280a0d721fe2fd
Suriyasak <suriyasak.chaisanam@gmail.com>
2025-07-09 11:03:11 +0700

    Change Security

---
+++ config.js
@@ -0,0 +1,4 @@
+// config.js
+const SUPABASE_URL = 'https://fhnprrlmlhleomfqqvpp.supabase.co';
+const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZobnBycmxtbGhsZW9tZnFxdnBwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTA5MTAyMjIsImV4cCI6MjA2NjQ4NjIyMn0.WA-_yNFWxpFnJBA3oh5UlOtq89KBm5hqsb51oi04hMk'; // <-- สำคัญ: ใส่ ANON KEY ที่ถูกต้อง
+        
