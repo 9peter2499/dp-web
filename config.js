650c438ab77d334d20a2a2c494b8ccdc7f2a916d
Suriyasak <suriyasak.chaisanam@gmail.com>
2025-08-07 22:54:25 +0700

    Check Error Log on Mobile Device

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 // config.js
-export const SUPABASE_URL = "https://supabase.dp-web.online";
+export const SUPABASE_URL = "https://fhnprrlmlhleomfqqvpp.supabase.co";
 export const SUPABASE_ANON_KEY =
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZobnBycmxtbGhsZW9tZnFxdnBwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTA5MTAyMjIsImV4cCI6MjA2NjQ4NjIyMn0.WA-_yNFWxpFnJBA3oh5UlOtq89KBm5hqsb51oi04hMk"; // <-- สำคัญ: ใส่ ANON KEY ที่ถูกต้อง
 
