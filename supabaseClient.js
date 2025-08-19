d91085482f8c3f40d19b915c52722b64dbc4c1a9
Suriyasak <suriyasak.chaisanam@gmail.com>
2025-08-19 13:02:49 +0700

    Change Login

--- supabaseClient.js
+++ supabaseClient.js
@@ -2,8 +2,12 @@
 import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2/+esm";
 import { SUPABASE_URL, SUPABASE_ANON_KEY } from "./config.js";
 
+const directSupabaseUrl = "https://fhnprrlmlhleomfqqvpp.supabase.co";
+
 // สร้างและตั้งค่า Supabase client ที่นี่ที่เดียว
-export const _supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY, {
+// SUPABASE_URL
+
+export const _supabase = createClient(directSupabaseUrl, SUPABASE_ANON_KEY, {
   global: {
     headers: { "x-client-info": "supabase-js/2.x" },
   },
